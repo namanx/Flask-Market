d9f4a2e77e43f01577d8e0309e3be9184c4fe5c7
Naman <namanwinner5@gmail.com>
2023-09-26 22:09:37 +0530

    added a template folder

--- src/market.py
+++ src/market.py
@@ -1,13 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "<h1>Namaste India</h1>"
+def HomePage():
+    return render_template('home.html')
 
-
-@app.route('/about')
-def about():
-    return "About us"
 if __name__ == "__main__":
     app.run(debug=True)
