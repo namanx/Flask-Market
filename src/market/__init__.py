aa3c6506c5b02af4405aae7098d75d6248fa9d2b
Naman <namanwinner5@gmail.com>
2023-09-30 15:29:48 +0530

    Added form using flask-wtf and wtform

--- src/market/__init__.py
+++ src/market/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-
+app.config['SECRET_KEY'] = 'bbc7d19bc0531e1c1805c237'
 app.app_context().push()
 
 db = SQLAlchemy(app)
--- src/market/routes.py
+++ src/market/routes.py
@@ -1,6 +1,7 @@
 from market import app
 from flask import render_template
 from market.models import Item
+from market.forms import RegisterForm
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -10,3 +11,8 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template('market.html',items=items,page='market')
+
+@app.route('/register')
+def register():
+    forms = RegisterForm()
+    return render_template('register.html',form = forms,page='register')
