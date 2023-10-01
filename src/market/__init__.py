63bba50144580075f1e3f3b67b597edd71bf313a
Naman <namanwinner5@gmail.com>
2023-10-01 12:05:19 +0530

    added a login form for the users

--- src/market/__init__.py
+++ src/market/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'bbc7d19bc0531e1c1805c237'
@@ -8,6 +8,7 @@ app.app_context().push()
 
 db = SQLAlchemy(app)
 
+bcrypt = Bcrypt(app)
 from market import models
 from market import routes
 
--- src/market/routes.py
+++ src/market/routes.py
@@ -1,7 +1,7 @@
 from market import app,db
 from flask import render_template, redirect, url_for,flash
 from market.models import Item,User
-from market.forms import RegisterForm
+from market.forms import RegisterForm,LoginForm
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -18,7 +18,7 @@ def register():
     if form.validate_on_submit():
         user_to_create = User(username = form.username.data,
                               email_address = form.email_address.data,
-                              password_hash = form.password1.data)
+                              password = form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
@@ -29,3 +29,8 @@ def register():
             flash(f'The error while creating user : {err_msg}',category='danger')
 
     return render_template('register.html',form = form,page='register')
+
+@app.route('/login',methods=['GET','POST'])
+def login_page():
+    form = LoginForm()
+    return render_template('login.html',page = 'login',form=form)
